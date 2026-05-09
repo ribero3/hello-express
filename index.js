797a92cea9152324d39f7d26038094d1b9b55ced
ribero3 <youto.200406@gmail.com>
2026-05-09 12:37:06 +0900

    test

--- index.js
+++ index.js
@@ -1,14 +1,5 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-app.post("/chat", (req, res) => {
-  const userMessage = req.body.message;
-  res.json({ reply: "AIっぽい返事: " + userMessage });
-});
-
 const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
 });
